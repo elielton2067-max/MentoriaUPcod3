5e6a97342b5f65ee9e87dbc9f99099276564d534
Elielton <elielton2067@gmail.com>
2026-09-05 17:35:34 -0300

    Revisão de condicionais

---
+++ Condicionais/Aula2.js
@@ -0,0 +1,16 @@
+let idade = 99
+if( idade >=0 && idade <=150){
+
+   if( idade >= 100 ){
+        console.log("Você é idoso")
+
+    }else if( idade >= 18){
+        console.log(" Maior de idade")
+    }
+    else{
+        console.log("Menor de idade")
+    }   
+}
+else{
+    console.log("Valor não válido")
+}
